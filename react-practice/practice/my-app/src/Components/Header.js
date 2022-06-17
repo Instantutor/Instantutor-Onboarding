a8d3cbe42bcc3a69587174200d8acbbb5a599f0c
jdawesomeguy <40191080+jdawesomeguy@users.noreply.github.com>
2022-06-17 03:15:49 -0400

    Added a Button Component
    Added a button component, not much detail to be said here. Still following the tutorial.

--- react-practice/practice/my-app/src/Components/Header.js
+++ react-practice/practice/my-app/src/Components/Header.js
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({title}) => {
   return (
-    <header>
+    <header className='header'>
         
         <h1>{title}</h1>
+        <Button color='green' text='Hello' />
+        <Button color='blue' text='Good Evening' />
+        <Button color='purple' text='Good Night' />
 
     </header>
   )
@@ -21,5 +25,14 @@ Header.propTypes = {
     //The program expects a string and won't go without one.
     title: PropTypes.string.isRequired,
 }
+
+//CSS in js
+// 
+// <h1 style={headingStyle}>{title}</h1>
+//
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
 
 export default Header
