c40766e35ec8e72b50b4dc154b938e18a9aa91e2
jdawesomeguy <40191080+jdawesomeguy@users.noreply.github.com>
2022-06-17 14:16:27 -0400

    Finishing up Header and Button
    Completed the Header and Button section

--- react-practice/practice/my-app/src/Components/Button.js
+++ react-practice/practice/my-app/src/Components/Button.js
@@ -1,7 +1,29 @@
-const Button = ({color, text}) => {
+import PropTypes from 'prop-types'
+
+const Button = ({color, text, onClick}) => {
+    // const onClick = (e) => {
+    //     console.log(e)
+
+    //     /* Passing the event object into the log to record where it is. */
+    // }
+
+
   return (
-    <button style={{ backgroundColor: color}} className='btn' >{text}</button>
+    <button onClick={onClick} style={{ backgroundColor: color}} 
+    className='btn' >
+        {text}
+    </button>
   )
+}
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
 }
 
 export default Button
--- react-practice/practice/my-app/src/Components/Header.js
+++ react-practice/practice/my-app/src/Components/Header.js
@@ -2,13 +2,18 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = ({title}) => {
+
+    const onClick = () => {
+        console.log('Click')
+    }
+
   return (
     <header className='header'>
         
         <h1>{title}</h1>
-        <Button color='green' text='Hello' />
-        <Button color='blue' text='Good Evening' />
-        <Button color='purple' text='Good Night' />
+        <Button color='green' text='Hello' onClick={onClick}/>
+        <Button color='blue' text='Good Evening' onClick={onClick}/>
+        <Button color='purple' text='Good Night' onClick={onClick}/>
 
     </header>
   )
