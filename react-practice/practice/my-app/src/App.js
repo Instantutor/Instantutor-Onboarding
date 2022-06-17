e0b2269702b8211e7186f15c2f9f2e9a5370bb98
jdawesomeguy <40191080+jdawesomeguy@users.noreply.github.com>
2022-06-17 15:16:32 -0400

    Continuing the tutorial, working on the tasks section.

--- react-practice/practice/my-app/src/App.js
+++ react-practice/practice/my-app/src/App.js
@@ -1,7 +1,30 @@
 import './App.css';
+import {useState} from 'react'
 import Header from './Components/Header'
+import Tasks from './Components/Tasks'
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Tutor Babies',
+        day: 'Feb 9th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Pet puppies',
+        day: 'Feb 12th at 1:30am',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Sleep',
+        day: 'Feb 14th at 7:30am',
+        reminder: false,
+    },    
+])
 
   const name = 'Dave'
   const x = 7
@@ -9,7 +32,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header></Header>
+      <Header/>
+      <Tasks tasks={tasks}/>
       <h1>Hello from James - React tutorial</h1>
       <h2>Hello {name} it has been {49 / x} years since you have turned me on.</h2>
       <h3>Sup, look at this if else: {y ? 'True' : 'False'} </h3>
