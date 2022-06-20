c9f5c6fa7173a7f29baa49d601c0c5731c21028b
sveludandi112 <sveludandi112@gmail.com>
2022-06-20 19:09:39 -0400

    Node-Crash-Course
    completed

---
+++ practice/Node-Crash-Course/person.js
@@ -0,0 +1,19 @@
+// Module Wrapper Function
+// (function (exports, require, module, __filename, __dirname)) {
+
+// })
+
+// console.log(__dirname, __filename);
+
+class Person {
+    constructor(name, age) {
+        this.name = name; 
+        this.age = age;
+    }
+
+    greeting() {
+        console.log('My name is %s and I am %d', this.name, this.age); 
+    }
+}
+
+module.exports = Person;
