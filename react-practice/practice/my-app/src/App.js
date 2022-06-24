4f9d6758c314583ea4282f340695e75f4f7f298c
jdawesomeguy <40191080+jdawesomeguy@users.noreply.github.com>
2022-06-24 13:43:59 -0400

    Finishing up Tutorial
    Doing the last commit for the tutorial stuff, moving on from this.

--- react-practice/practice/my-app/src/App.js
+++ react-practice/practice/my-app/src/App.js
@@ -27,6 +27,12 @@ function App() {
     },    
 ])
 
+  // Add Task
+
+  const addTask = (task) => {
+    console.log(task);
+  }
+
   // Creating a delete funtion
 
   const deleteTask = (id) => {
@@ -47,7 +53,7 @@ function App() {
   return (
     <div className="container">
       <Header/>
-      <AddTask />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete=
         {deleteTask} onToggle = {toggleReminder}/>
